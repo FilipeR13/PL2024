3ffeca8a80244c02d0a3f0f8c75a3ad81184d32a
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-03-13 17:06:53 +0000

    Completed TPC5

--- TPC5/README.md
+++ TPC5/README.md
@@ -7,7 +7,7 @@
 
 ## Resumo
 
-Este programa, desenvolvido em Python, simula o comportamento de uma máquina de vendas. Ele permite que os utilizadores interajam com a máquina inserindo comandos para listar produtos, adicionar moedas, selecionar produtos para compra e sair do mesmo.
+Este programa, desenvolvido em Python, simula o comportamento de uma máquina de vendas. Ele permite que os utilizadores interajam com a máquina inserindo comandos para listar produtos, adicionar moedas, selecionar produtos para compra, adicionar produtos ao stock e sair do mesmo.
 
 ## Resolução
 
@@ -20,6 +20,7 @@ Foram criados vários tokens para cada operação possível na máquina de venda
 1. `LISTAR`
 2. `MOEDA`
 3. `SELECIONAR`
+3. `ADICIONAR`
 4. `SAIR`
 
 Cada token tem associado uma expressão regular associada permitindo identificar e classificar corretamente as entradas do utilizador.
@@ -47,24 +48,53 @@ python3 main.py lista.json
 ### Lista de Comandos
 
 - **LISTAR:** Exibir todos os produtos na máquina de vendas.
+  - Argumentos: Não possui
 - **MOEDA:** Adicionar moedas à máquina.
+  - Argumentos: Moedas (Lista de moedas separado por `,` (ex: "1e,20c"))
 - **SELECIONAR:** Selecionar um produto para compra.
+  - Argumentos: Id
+- **ADICIONAR:** Adicionar produtos à máquina, existentes ou não existentes.
+  - Argumentos: Nome Quantidade [preco](caso o produto seja novo)
 - **SAIR:** Sair da simulação e receber troco.
+  - Argumentos: Não possui
 
 ## Exemplo de Uso
 
 ```
+Bem-vindo à máquina de venda automática
 $ listar
-     Número     |            Nome                              |       Preço
-       1        |        Agua                                  |       0.6
-       2        |        Coca-Cola                             |       0.9
-       3        |        Palmier                               |       0.7
-       3        |        Croassant Misto                       |       1.2
-$ moeda 1e
-Saldo: 1.0€
+     Número     |            Nome                              |       Preço     |      Quantidade
+       1        |        Agua                                  |       0.6       |       10
+       2        |        Coca-Cola                             |       0.9       |       1
+       3        |        Palmier                               |       0.7       |       10
+       3        |        Croassant Misto                       |       1.2       |       10
+$ adicionar fanta 3 0.8
+maq: Adicionada quantidade 3 ao produto fanta
+$ listar
+     Número     |            Nome                              |       Preço     |      Quantidade
+       1        |        Agua                                  |       0.6       |       10
+       2        |        Coca-Cola                             |       0.9       |       1
+       3        |        Palmier                               |       0.7       |       10
+       3        |        Croassant Misto                       |       1.2       |       10
+       5        |        fanta                                 |       0.8       |       3
+$ moeda 1e,50c
+maq: Saldo: 1.5€
 $ selecionar 2
-Produto Coca-Cola comprado com sucesso
+maq: Produto Coca-Cola comprado com sucesso
+$ selecionar 2
+maq: Produto com id 2 esgotado
+$ selecionar 3
+maq: Saldo insuficiente
+maq: Saldo=0.6€; Preço=1.2€
+$ listar
+     Número     |            Nome                              |       Preço     |      Quantidade
+       1        |        Agua                                  |       0.6       |       10
+       2        |        Coca-Cola                             |       0.9       |       0
+       3        |        Palmier                               |       0.7       |       10
+       3        |        Croassant Misto                       |       1.2       |       10
+       5        |        fanta                                 |       0.8       |       3
 $ sair
-Troco: 0.1
+maq: Troco: 0.6
+0.5€: 1
 0.1€: 1
 ```
--- TPC5/main.py
+++ TPC5/main.py
@@ -6,7 +6,8 @@ tokens = [
     'LISTAR',
     'MOEDA',
     'SAIR',
-    'SELECIONAR'   
+    'SELECIONAR',
+    'ADICIONAR',
 ]
 
 def t_LISTAR(t):
@@ -23,6 +24,10 @@ def t_SAIR(t):
 
 def t_SELECIONAR(t):
     r'selecionar[ ]\d+'
+    return t
+
+def t_ADICIONAR(t):
+    r'adicionar[ ][\w-]+[ ]\d([ ]\d+(\.\d+)?)?'
     return t
 
 t_ignore = '\t\n'
@@ -57,26 +62,26 @@ def main(argv):
     lexer = lex.lex()
 
     saldo = 0
-
+    print("Bem-vindo à máquina de venda automática")
     for line in sys.stdin:
         lexer.input(line)
         for token in lexer:
             if not token: break 
             if token.type == "LISTAR":
-                print('     Número     |            Nome                              |       Preço')
+                print('     Número     |            Nome                              |       Preço     |      Quantidade')
                 for produto in produtos:
-                    print(f'       {produto["id"]}        |        {produto["nome"]: <30}        |       {produto["preco"]}')
+                    print(f'       {produto["id"]}        |        {produto["nome"]: <30}        |       {produto["preco"]}       |       {produto["quantidade"]}')
             elif token.type == "MOEDA":
                 moedas = token.value.split(" ")[1].split(",")
                 for moeda in moedas:
                     if moeda[-1] == "e":
                         saldo += float(moeda[:-1])
                     else:
-                        saldo += float(moeda[:-1]) / 100
-                print(f"Saldo: {saldo}€")
+                        saldo += round(float(moeda[:-1]) / 100,2)
+                print(f"maq: Saldo: {saldo}€")
             
             elif token.type == "SAIR":
-                print(f"Troco: {round(saldo,2)}")
+                print(f"maq: Troco: {round(saldo,2)}")
                 change = get_change(saldo)
                 for coin in change:
                     if change[coin] > 0:
@@ -85,15 +90,32 @@ def main(argv):
             elif token.type == "SELECIONAR":
                 id = int(token.value.split(" ")[1])
                 if id in tabela:
-                    produto = tabela[id]
-                    if saldo >= produto["preco"]:
-                        saldo -= produto["preco"]
-                        print(f"Produto {produto['nome']} comprado com sucesso")
-                    else:
-                        print("Saldo insuficiente")
+                    if tabela[id]["quantidade"] > 0:    
+                        produto = tabela[id]
+                        if saldo >= produto["preco"]:
+                            saldo -= produto["preco"]
+                            tabela[id]["quantidade"] -= 1
+                            print(f"maq: Produto {produto['nome']} comprado com sucesso")
+                        else:
+                            print(f"maq: Saldo insuficiente\nmaq: Saldo={saldo}€; Preço={produto['preco']}€")
+                    else :
+                        print(f"maq: Produto com id {id} esgotado")
                 else:
-                    print(f"Produto com id {id} não existe")
-
+                    print(f"maq: Produto com id {id} não existe")
+            elif token.type == "ADICIONAR":
+                nome = token.value.split(" ")[1]
+                quantidade = int(token.value.split(" ")[2])
+                for produto in produtos:
+                    if produto["nome"] == nome:
+                        produto["quantidade"] += quantidade
+                        break
+                else:
+                    if len(token.value.split(" ")) == 4:
+                        preco = float(token.value.split(" ")[3])
+                        produtos.append({"id": len(produtos)+1, "nome": nome, "preco": preco, "quantidade": quantidade})
+                    else:
+                        print(f"maq: Produto {nome} não existe e não foi dado um preço para o novo produto")
+                print(f"maq: Adicionada quantidade {quantidade} ao produto {nome}")
 
 if __name__ == "__main__":
     main(sys.argv)
